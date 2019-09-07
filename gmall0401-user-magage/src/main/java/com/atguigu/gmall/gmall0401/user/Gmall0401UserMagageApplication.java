8196135f0153103d9a2439795efe000e7aa427a9
KungFubgzYang <283356173@qq.com>
2019-09-07 21:43:41 +0800

    加入用户管理模块

---
+++ gmall0401-user-magage/src/main/java/com/atguigu/gmall/gmall0401/user/Gmall0401UserMagageApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall.gmall0401.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0401UserMagageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0401UserMagageApplication.class, args);
+    }
+
+}
